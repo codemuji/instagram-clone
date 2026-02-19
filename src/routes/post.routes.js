58bfb1e56187be4c5da4e235aa9dcd63c6d89a52
codemuji <rajatpradhan011@gmail.com>
2026-02-19 08:03:19 +0530

    Implement user authentication middleware and enhance post controller with detailed post retrieval

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,12 +3,29 @@ const postRouter = express.Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const identifyUser = require("../middlewares/auth.middleware");
 
 /**
  * POST/api/post [protected]
  * req.body = {caption,image-file}
  */
 
-postRouter.post("/",upload.single("image"), postController.createPostController);
+postRouter.post(
+  "/",
+  upload.single("image"),
+  identifyUser,
+  postController.createPostController,
+);
+
+/**
+ * GET /api/posts/ [protected]
+ */
+
+postRouter.get("/", identifyUser, postController.getPostController);
+
+/**
+ * GET /api/posts/details/:postId
+ */
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetails);
 
 module.exports = postRouter;
